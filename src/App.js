e4bb036cb0e900edcfbf6ac8efb6a404d65f0e66
Eduardo <educcarnn@gmail.com>
2022-07-06 17:36:31 -0300

    Feat: Adicionando toast container na app js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,23 @@ import './App.css';
 import Routes from './routes';
 import { Global } from './styles/GlobalStyle';
 import { Reset } from './styles/Reset';
+import { ToastContainer } from 'react-toastify';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <ToastContainer
+        position="top-right"
+        autoClose={1200}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        />
         <Global/>
         <Reset/>
         <Routes/>
