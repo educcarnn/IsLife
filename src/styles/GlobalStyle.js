9ee774b4bff61eae5a7ea535cea715167e86e0b9
Eduardo <educcarnn@gmail.com>
2022-07-06 18:04:06 -0300

    Feat: Padronização cores e globalstyle

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -3,6 +3,9 @@ import { createGlobalStyle } from "styled-components"
 export const Global = createGlobalStyle`
 body {
     box-sizing: border-box;
+    background-color: #F5F5F5;
+    color: black;
+    font-style: 'Inter', sans-serif;
 }
 button{
     cursor: pointer;
