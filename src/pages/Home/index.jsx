ef68b792837f9331143f4754750ac5da40b3bdf5
Eduardo <educcarnn@gmail.com>
2022-07-06 11:07:04 -0300

    Feat: Estrutura inicial dos routes e pages

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,8 @@
+function Home(){
+    return(
+        <div>Seja bem vindo ao lugar que une paciente e médico em uma mesma sintonia</div>
+    )
+
+}
+
+export default Home
---
+++ src/routes/index.js
@@ -0,0 +1,16 @@
+import { Switch, BrowserRouter, Route } from "react-router-dom";
+import Home from "../pages/Home";
+
+const Routes = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <Home/>
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
+};
+
+export default Routes;
