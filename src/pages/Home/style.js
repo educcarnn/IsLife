619b4aec9e789e8d0566490837e2f9ca48a7288d
anjosdelacerda <anjosdelacerda@gmail.com>
2022-07-08 17:44:43 -0300

    aqui está na versão antiga da Home, tanto index quanto style, porém não fiz pull da atualizada ainda que o eduardo me mostrou, então não faça a mesclagem

--- src/pages/Home/style.js
+++ src/pages/Home/style.js
@@ -1,6 +1,5 @@
 import styled from "styled-components";
 
-<<<<<<< HEAD
 export const Container = styled.section`
   width: 100vw;
   height: 100vh;
@@ -109,98 +108,5 @@ export const ImgContainer = styled.div`
   img {
     width: 34rem;
     height: 90%;
-=======
-export const ContainerHome = styled.main`
-  width: 100%;
-  height: 100vh;
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-  margin-right: 16px;
-  @media (max-width: 800px) {
-    display: flex;
-    flex-direction: column;
-  }
-`;
-
-export const ContentWelcome = styled.div`
-  width: 500px;
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
-  align-items: center;
-  align-content: center;
-
-  .div-logo--home {
-    width: 200px;
-    height: 200px;
-    object-fit: contain;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-
-    img {
-      width: 350px;
-    }
-  }
-  .welcome {
-    h3 {
-      font-size: 42px;
-      font-weight: 700;
-      font-family: "Sofia Sans";
-      margin-bottom: 25px;
-    }
-    h2 {
-      font-size: 60px;
-      font-weight: 700;
-      font-family: "Sofia Sans";
-      margin-bottom: 25px;
-    }
-    .text-line {
-      font-size: 25px;
-      font-weight: 700;
-      font-family: "Sofia Sans";
-      margin-bottom: 25px;
-      overflow: hidden;
-    }
-  }
-  .div-buttons--home {
-    display: flex;
-    flex-direction: row;
-    width: 310px;
-    gap: 16px;
-  }
-  @media (max-width: 800px) {
-    width: 90%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-
-    .div-logo--home {
-      width: 200px;
-      height: 200px;
-      margin-top: 16px;
-      object-fit: contain;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-    }
-    img {
-      width: 350px;
-    }
-  }
-`;
-
-export const ContentImg = styled.div`
-  width: 500px;
-
-  display: flex;
-  img {
-    width: 100%;
-  }
-  @media (max-width: 800px) {
-    display: none;
->>>>>>> bbf6bf842caad2b3cf2a771ea6ef3b50af71129f
   }
 `;
--- src/routes/index.js
+++ src/routes/index.js
@@ -8,19 +8,20 @@ import DashPatient from "../pages/DashPatient";
 import DoctorPatient from "../pages/DoctorPatient";
 import DoctorSchedule from "../pages/DoctorSchedule";
 import Home from "../pages/Home";
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from "react";
 
 const Routes = () => {
-  
   const [auth, setAuth] = useState(false);
 
-    useEffect(() => {
-        const token = localStorage.getItem('@IsLife:token');
+  console.log(auth);
 
-        if (token) {
-            return setAuth(true)
-        };
-    },[]);
+  useEffect(() => {
+    const token = localStorage.getItem("@IsLife:token");
+
+    if (token) {
+      return setAuth(true);
+    }
+  }, []);
 
   return (
     <BrowserRouter>
@@ -35,9 +36,6 @@ const Routes = () => {
           <AuthPatient />
         </Route>
         <Route exact path="/dashdoctor">
-<<<<<<< HEAD
-          <DashDoctor />
-=======
           <HeaderDoctor />
           <DashDoctor />
         </Route>
@@ -48,7 +46,6 @@ const Routes = () => {
         <Route exact path="/doctorpatient">
           <HeaderDoctor />
           <DoctorPatient />
->>>>>>> bbf6bf842caad2b3cf2a771ea6ef3b50af71129f
         </Route>
         <Route exact path="/dashpatient">
           <DashPatient />
