2e6024c4556fcdbf1dde20009589b8f7e1befab5
Eduardo <educcarnn@gmail.com>
2022-07-11 17:45:19 -0300

    Feat: Home paciente

--- src/pages/HomePatient/index.jsx
+++ src/pages/HomePatient/index.jsx
@@ -1,15 +1,21 @@
 import { useHistory } from "react-router-dom"
+import undraw from "../../img/undraw.svg"
+import logo from "../../img/islife.png"
+import { DivHomePatient } from "./style"
+
 function HomePatient(){
 
     const history = useHistory()
     return (
-        <div>
+        <DivHomePatient>
+            <img src={logo} alt={logo} className="imgLogo"></img>
             <div>Feito para ficar mais próximo do seu médico</div>
-            <div>
+            <img src={undraw} alt={undraw} className="imgHome"></img>
+            <div className="DivButtonHome">
                 <span>Realize aqui seu login</span>
+                <button onClick={() => history.push("/login")}>Ir para login</button> 
             </div>
-            <button onClick={() => history.push("/login")}>Ir para login</button> 
-        </div>
+        </DivHomePatient>
     )
 
 }
---
+++ src/pages/HomePatient/style.js
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const DivHomePatient = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin-top: 2rem;
+    gap: 3rem;
+    align-items: center;
+    background-color: darkturquoise;
+    padding-bottom: 7%;
+    padding-top: 1.7%;
+   
+
+
+    .imgLogo{
+        width: 7rem;
+        margin-bottom: -1rem;
+    }
+    .imgHome{
+        width: 12rem;
+    }
+    .DivButtonHome{
+    display: flex;
+    flex-direction: column;
+    background-color: #D9D9D9;
+    padding: 1rem;
+    border-radius: 5px;
+    }
+`
