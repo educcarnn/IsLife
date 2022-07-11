3da06517b4ef0c5531bdb0abd80bdab9675f2862
herbertcaiosama <herberttt@gmail.com>
2022-07-11 11:49:09 -0300

    feat: estilização home completa

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,63 +1,118 @@
-import {
-  Container,
-  ContainerVisual,
-  Menu,
-  Welcome,
-  ImgContainer,
-  Nav,
-} from "./style";
-import ButtonForm from "../../components/button";
+import logo from "../../img/IsLifeLogo1.png";
+import ImgHome from "../../img/DashHome.png";
+import Fernando from "../../img/Fernando.jfif";
+import Idaline from "../../img/Idaline.jfif";
+import Leomar from "../../img/Leomar.jfif";
+import Lucas from "../../img/Lucas.jfif";
+import Eduardo from "../../img/Eduardo.jfif";
+import Herbert from "../../img/Herbert.jpg";
+import { AiFillGithub } from "react-icons/ai";
+import { AiFillLinkedin } from "react-icons/ai";
+import { AiOutlineInstagram } from "react-icons/ai";
+import { useHistory } from "react-router";
+import { Container, Logo, WelcomeContainer, Footer } from "./style";
 
-import Logo from "../../img/IsLifeLogo.png";
-import ImgHome from "../../img/DashHome.svg";
+function Home () {
+    const history = useHistory();
 
-import { useHistory } from "react-router-dom";
+    function goToDash(path) {
+        history.push(path);
+      }
 
-function Home() {
-  const history = useHistory();
-
-  function goToDash(path) {
-    history.push(path);
-  }
-
-  return (
-    <>
-      <Container>
-        <Nav>
-          <div>
-            <img src={Logo} alt="IsLife" />
-          </div>
-        </Nav>
-        <ContainerVisual>
-          <Welcome>
-            <div>
-              <h2>Seja bem vindo(a)!</h2>
-              <h1>
-                Is<span>Life</span>
-              </h1>
-            </div>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
-              bibendum commodo massa. Donec neque lectus, efficitur nec porta
-              non, faucibus quis enim.
-            </p>
-
-            <Menu>
-              <ButtonForm onClick={() => goToDash("/home/doctor")}>
-                Sou um profissional
-              </ButtonForm>
-              <ButtonForm onClick={() => goToDash("/home/patient")}>
-                Sou um cliente
-              </ButtonForm>
-            </Menu>
-          </Welcome>
-          <ImgContainer>
-            <img src={ImgHome} alt="IsLife" />
-          </ImgContainer>
-        </ContainerVisual>
-      </Container>
-    </>
-  );
-}
+    return(
+        <>
+            <Logo>
+                <img src={logo} alt="IsLife" />
+            </Logo>
+            <Container>
+                <WelcomeContainer>
+                    <h1>Seja Bem Vindo (a)</h1>
+                    <h2>IsLife</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam bibendum commodo massa.
+                        Donec neque lectus, efficitur nec porta non, faucibus quis enim.
+                    </p>
+                    <div>
+                        <button onClick={() => goToDash("/home/doctor")}>Sou um médico</button>
+                        <button onClick={() => goToDash("/home/patient")}>Sou um paciente</button>
+                    </div>
+                </WelcomeContainer>
+                <div>
+                    <img src={ImgHome} alt="Welcome" />
+                </div>
+            </Container>
+            <Footer>
+                <h3>Conheça os devs que fizeram esse projeto</h3>
+                <ul>
+                    <li>
+                        <img src={Eduardo} alt="Eduardo" />
+                        <h2>Eduardo Carneiro</h2>
+                        <h4>Dev</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris purus erat, tincidunt nec pulvinar vel, consectetur id tellus.</p>
+                        <div className="icons">
+                            <AiFillGithub className="github" />
+                            <AiFillLinkedin className="linkedin" />
+                            <AiOutlineInstagram  className="instagram" />
+                        </div>
+                    </li>
+                    <li>
+                        <img src={Idaline} alt="Idaline" />
+                        <h2>Idaline Viana</h2>
+                        <h4>Dev</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris purus erat, tincidunt nec pulvinar vel, consectetur id tellus.</p>
+                        <div className="icons">
+                            <AiFillGithub className="github" />
+                            <AiFillLinkedin className="linkedin" />
+                            <AiOutlineInstagram  className="instagram" />
+                        </div>
+                    </li>
+                    <li>
+                        <img src={Fernando} alt="Fernando" />
+                        <h2>Fernando Lacerda</h2>
+                        <h4>Dev</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris purus erat, tincidunt nec pulvinar vel, consectetur id tellus.</p>
+                        <div className="icons">
+                            <AiFillGithub className="github" />
+                            <AiFillLinkedin className="linkedin" />
+                            <AiOutlineInstagram  className="instagram" />
+                        </div>
+                    </li>
+                    <li>
+                        <img src={Herbert} alt="Herbert" />
+                        <h2>Hebert Sipriano</h2>
+                        <h4>Dev</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris purus erat, tincidunt nec pulvinar vel, consectetur id tellus.</p>
+                        <div className="icons">
+                            <AiFillGithub className="github" />
+                            <AiFillLinkedin className="linkedin" />
+                            <AiOutlineInstagram  className="instagram" />
+                        </div>
+                    </li>
+                    <li>
+                        <img src={Leomar} alt="Leomar" />
+                        <h2>Leomar Paz</h2>
+                        <h4>Dev</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris purus erat, tincidunt nec pulvinar vel, consectetur id tellus.</p>
+                        <div className="icons">
+                            <AiFillGithub className="github" />
+                            <AiFillLinkedin className="linkedin" />
+                            <AiOutlineInstagram  className="instagram" />
+                        </div>
+                    </li>
+                    <li>
+                        <img src={Lucas} alt="Lucas" />
+                        <h2>Lucas Roque</h2>
+                        <h4>Dev</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris purus erat, tincidunt nec pulvinar vel, consectetur id tellus.</p>
+                        <div className="icons">
+                            <AiFillGithub className="github" />
+                            <AiFillLinkedin className="linkedin" />
+                            <AiOutlineInstagram  className="instagram" />
+                        </div>
+                    </li>
+                </ul>
+            </Footer>
+        </>
+    );
+};
 
 export default Home;
--- src/pages/Home/style.js
+++ src/pages/Home/style.js
@@ -1,112 +1,297 @@
 import styled from "styled-components";
 
-export const Container = styled.section`
-  width: 100vw;
-  height: 100vh;
+export const Logo = styled.header`
+  position: relative;
+  top: -100px;
 
+  img {
+    width: 375px;
+    height: 350px;
+  }
+
+  @media screen and (min-width: 768px) {
+    top: -110px;
+    left: 90px;
+
+    img {
+      width: 600px;
+      height: 450px;
+    }
+  }
+
+  @media screen and (min-width: 1024px) {
+    top: -60px;
+    left: 50px;
+
+    img {
+      width: 381px;
+      height: 350px;
+    }
+  }
+`;
+
+export const Container = styled.main`
+  display: flex;
+  justify-content: space-between;
+
+  img {
+    width: 375px;
+    height: 180px;
+    position: absolute;
+    top: 150px;
+    left: 0;
+    border-radius: 60px;
+  }
+
+  button {
+    width: 320px;
+    height: 65px;
+    background-color: #ce2d5d;
+    font-weight: 700;
+    font-size: 1.7rem;
+    color: #ffffff;
+    border: none;
+    margin-top: 10px;
+    margin-left: 28px;
+    border-radius: 50px;
+    transition: 0.6s;
+  }
+  & button:hover {
+    background-color: #e21955;
+    font-size: 1.85rem;
+  }
+
+  @media screen and (min-width: 768px) {
+    img {
+      width: 760px;
+      height: 350px;
+      top: 220px;
+    }
+
+    button {
+      margin-left: 10px;
+      margin-right: 10px;
+    }
+  }
+
+  @media screen and (min-width: 1024px) {
+    img {
+      width: 560px;
+      height: 390px;
+      position: relative;
+      top: -225px;
+      right: 95px;
+    }
+
+    button {
+      width: 201px;
+      height: 47px;
+      font-size: 1.188rem;
+      margin-right: 5px;
+    }
+    & button:hover {
+      font-size: 1.25rem;
+    }
+  }
+`;
+
+export const WelcomeContainer = styled.div`
   display: flex;
   flex-direction: column;
-
   align-items: center;
-
-  background: var(--brancoBackground);
-`;
-
-export const Nav = styled.nav`
-  display: flex;
-  width: 100vw;
-  height: 8rem;
-
-  align-items: center;
-
-  background: var(--brancoBackground);
-`;
-
-export const ContainerVisual = styled.div`
-  width: 100vw;
-  /* height: 100vh; */
-
-  display: flex;
-
-  align-items: center;
-  justify-content: center;
-
-  background: var(--brancoBackground);
-`;
-
-export const Welcome = styled.div`
-  display: flex;
-  flex-direction: column;
-
-  width: 35rem;
-  height: 100%;
-
-  justify-content: space-evenly;
+  position: relative;
+  top: 10px;
+  margin-bottom: 30px;
 
   h1 {
-    font-family: "Roboto", serif;
-    font-size: 3rem;
-    font-weight: bold;
-
-    color: var(--pretoTitles);
+    font-size: 2.42rem;
+    font-weight: 700;
+    line-height: 50px;
   }
 
   h2 {
-    font-family: "Roboto", serif;
-    font-size: 2rem;
-
-    color: var(--pretoTitles);
-
-    margin-bottom: 1rem;
+    font-size: 3.75rem;
+    font-weight: 700;
+    line-height: 72px;
+    margin-top: 10px;
   }
 
   p {
-    font-family: "Inter", serif;
-    font-size: 1rem;
-
-    color: var(--pretoFont);
+    font-size: 1.75rem;
+    font-weight: 500;
+    line-height: 33px;
+    margin: 10px 0 20px 0;
+    padding: 0 28px 0 28px;
+    text-align: justify;
   }
 
-  span {
-    color: var(--rosa);
+  @media screen and (min-width: 768px) {
+    top: 160px;
+    margin-bottom: 230px;
+  }
+
+  @media screen and (min-width: 1024px) {
+    top: -160px;
+
+    h1 {
+      font-size: 2.3rem;
+    }
+
+    h2 {
+      font-size: 3.2rem;
+      margin-top: 1px;
+    }
+
+    p {
+      font-size: 1.3rem;
+      line-height: 33px;
+      margin: 1px 0 10px 0;
+      text-align: start;
+    }
   }
 `;
 
-export const Menu = styled.div`
-  width: 30rem;
-  height: 4rem;
-
-  display: flex;
-  flex-direction: row;
-
-  justify-content: center;
-  align-items: center;
-
-  button {
-    font-size: 1rem;
+export const Footer = styled.footer`
+  h3 {
+    font-weight: 600;
+    font-size: 1.8rem;
+    line-height: 33px;
+    padding-bottom: 16.5px;
+    padding-left: 60px;
+    padding-right: 28px;
   }
 
-  button + button {
-    margin-left: 2rem;
+  ul {
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    padding-top: 32px;
+    border-top: 1px solid #000000;
   }
 
-  /* background: var(--rosa);
-  color: var(--backgroundInputs); */
-`;
-
-export const ImgContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-
-  width: 35rem;
-  height: 100%;
-
-  align-items: center;
-
-  justify-content: center;
+  li {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 375px;
+    height: 450px;
+    background-color: #d9d9d9;
+    border-radius: 10px;
+    margin: 0 28px 28px 28px;
+  }
 
   img {
-    width: 34rem;
-    height: 90%;
+    border-radius: 50%;
+    margin: 25px 0 16px 0;
+  }
+
+  h2 {
+    font-weight: 700;
+    font-size: 1.875rem;
+    line-height: 36px;
+    margin-bottom: 12px;
+  }
+
+  h4 {
+    font-weight: 600;
+    font-size: 1.875rem;
+    line-height: 36px;
+    margin-bottom: 16px;
+  }
+
+  p {
+    font-weight: 500;
+    font-size: 1.25rem;
+    line-height: 24px;
+    padding: 0 50px 0 50px;
+  }
+
+  .icons {
+    cursor: pointer;
+    margin-top: 20px;
+  }
+
+  .github {
+    width: 40px;
+    height: 40px;
+    padding: 10px;
+    transition: 0.8s;
+  }
+  & .github:hover {
+    width: 50px;
+    height: 50px;
+  }
+
+  .linkedin {
+    width: 40px;
+    height: 40px;
+    padding: 10px;
+    transition: 0.8s;
+  }
+  & .linkedin:hover {
+    width: 50px;
+    height: 50px;
+  }
+
+  .instagram {
+    width: 40px;
+    height: 40px;
+    padding: 10px;
+    transition: 0.8s;
+  }
+  & .instagram:hover {
+    width: 50px;
+    height: 50px;
+  }
+
+  @media screen and (min-width: 768px) {
+    h3 {
+      padding-left: 28px;
+    }
+
+    ul {
+      padding-top: 5px;
+    }
+
+    li {
+      width: 328px;
+      height: 430px;
+      margin: 28px;
+    }
+
+    .icons {
+      cursor: pointer;
+      margin-top: 7px;
+    }
+  }
+
+  @media screen and (min-width: 1024px) {
+    margin-top: -250px;
+
+    h3 {
+      font-size: 2rem;
+    }
+
+    ul {
+      padding-top: 32px;
+    }
+
+    li {
+      width: 265px;
+      height: 420px;
+      padding: 10px;
+    }
+
+    p {
+      font-weight: 500;
+      font-size: 1.18rem;
+      line-height: 22px;
+      padding: 0 20px 0 20px;
+      text-align: center;
+    }
+
+    .icons {
+      margin-top: 10px;
+    }
   }
 `;
