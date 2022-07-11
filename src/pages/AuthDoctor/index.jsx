57feaf13d25ee844bf4e50fcfe02ec66b79a83d9
Eduardo <educcarnn@gmail.com>
2022-07-11 15:29:56 -0300

    Fix: Informações do registro

--- src/pages/AuthDoctor/index.jsx
+++ src/pages/AuthDoctor/index.jsx
@@ -1,35 +1,24 @@
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
-
-import axios from "axios";
-
-// import Input from "../../components/inputs";
-import { Input } from "../../components/inputs";
-import { Select } from "../../components/inputs";
-import ButtonForm from "../../components/button";
-
+import { api } from "../../services/api";
+import logo from "../../img/islife.png"
 import { useHistory } from "react-router-dom";
-
-import { DivGlobalLogin, FormLoginDoctor, Nav } from "./style";
-
-import Logo from "../../img/IsLifeLogo.png";
-
+import { DivElems, FormElem } from "./style";
 function AuthDoctor() {
-  const history = useHistory();
+  const history = useHistory()
 
   let schema = yup.object().shape({
     name: yup.string().required("Adicione valores válidos"),
     cpf: yup.string().required("CPF Inválido"),
     email: yup.string().required("campo obrigatório").email("e-mail inválido!"),
-    confirmedPassword: yup
+    confirmPassword: yup
       .string()
       .required("confirmação obrigatória")
       .oneOf([yup.ref("password")], "senha desigual!"),
     telefone: yup.string().required("informe o seu telefone"),
     profissão: yup.string().required("selecione um dos campos"),
     crm: yup.string().required("documentação obrigatória"),
-    cnpj: yup.string().required("campo obrigatório"),
   });
 
   const {
@@ -38,109 +27,86 @@ function AuthDoctor() {
     formState: { errors },
   } = useForm({ resolver: yupResolver(schema) });
 
-  const onSubmitFunction = (data) => {
+  console.log(errors)
+  const sendDates = (info) => {
+   
+    const dates = {
+      name: info.name,
+      age: info.age,
+      cpf: info.cpf,
+      telefone: info.telefone,
+      email: info.email,
+      password: info.password, 
+      type: "doctor",
+      crm: info.crm,
+      profissão: info.profissão,
+      cnpj: '00000'
+    };
 
-
-    data.type = "doctor";
-    data.patients = [];
-
-    axios
-      .post("https://is-life-fake-api.herokuapp.com/register", data)
-      .then((response) => {
-        console.log(response);
-        console.log(data);
-        localStorage.setItem(
-          "token",
-          JSON.stringify(response.data.accessToken)
-        );
-        localStorage.setItem(
-          "@isLifeDoctor:",
-          JSON.stringify(response.data.user)
-        );
-        response.data.user.type === "patient"
-          ? history.push("/dashpatient")
-          : history.push("/dashdoctor");
-      })
+    api
+      .post("/register", dates)
+      .then((res) => console.log(res))
       .catch((err) => console.log(err));
   };
 
   return (
-    <DivGlobalLogin>
-      <Nav>
-        <div>
-          <img src={Logo} alt="IsLife" />
-        </div>
-      </Nav>
 
-      <FormLoginDoctor onSubmit={handleSubmit(onSubmitFunction)}>
-        <Input
-          label="Nome"
-          placeholder="Nome completo"
-          register={register}
-          name="name"
-        />
-        <span> {errors.name && errors.name.message} </span>
-        <Input label="CPF" placeholder="CPF" register={register} name="cpf" />
-        <span> {errors.cpf && errors.cpf.message} </span>
+  <FormElem className="formItem" onSubmit={handleSubmit(sendDates) }>
+      <button onClick={() => history.push("/")} className="buttonDoctor">
+        <img src={logo} alt={logo} className="imgSize"></img>
+      </button>
+      
+      <div className="DivElem">
+        <DivElems>
+          <label>Nome</label>
+          <input
+            {...register("name")}
+            placeholder="Digite aqui seu nome"
+          className="label"></input>
+        </DivElems>
+        <DivElems>
+          <label>Data de nascimento</label>
+          <input {...register("age")} type='date' className="label"></input>
+        </DivElems>
+        <DivElems>
+          <label>CPF</label>
+          <input placeholder="Digite aqui seu CPF" {...register("cpf")} className="label"></input>
+        </DivElems>
+        <DivElems>
+          <label>Telefone</label>
+          <input placeholder="Digite seu telefone" {...register('telefone')} className="label"></input>
+        </DivElems>
+        <DivElems>
+          <label>E-mail</label>
+          <input placeholder="Digite aqui seu e-mail" {...register('email')} className="label" type='password' required></input>
+        </DivElems>
+        <DivElems>
+          <label>Senha</label>
+          <input placeholder="Digite aqui sua senha" {...register('password')} className="label" type='password' required></input>
+        </DivElems>
+        <DivElems>
+          <label>Confirmar sua senha</label>
+          <input placeholder="Confirme sua senha" {...register('confirmPassword')} className="label"></input>
+        </DivElems>
+        <DivElems>
+          <label>CRM</label>
+          <input placeholder="Digite seu CRM" {...register('crm')} className="label"></input>
+        </DivElems>
+        <DivElems>
+          <label>Escolha sua profissão </label>
+          <select {...register('profissão')} className="label">
+            <option value="psicóloga">Psicóloga(o)</option>
+            <option value="psiquiatra">Psiquiatra</option>
+            <option value="outros">Outros</option>
+          </select>
+        </DivElems>
+      </div>
+      <div className="DivButton">
+        <button type="submit">Cadastrar</button>
+      </div>
+    </FormElem>
 
-        <Input
-          label="E-mail"
-          placeholder="E-mail"
-          register={register}
-          name="email"
-        />
-        <span> {errors.email && errors.email.message} </span>
-        <Input
-          register={register}
-          name="password"
-          label="Senha"
-          type="password"
-          placeholder="Senha"
-        />
-        <span> {errors.password && errors.password.message}</span>
-        <Input
-          label="Telefone"
-          placeholder="Telefone"
-          register={register}
-          name="telefone"
-        />
-        <span> {errors.telefone && errors.telefone.message} </span>
-        <Input
-          label="Confirme sua senha"
-          type="password"
-          placeholder="Confirme sua senha"
-          register={register}
-          name="confirmedPassword"
-        />
-        <span>
-          {" "}
-          {errors.confirmedPassword && errors.confirmedPassword.message}{" "}
-        </span>
-
-        <Select label="Profissão" register={register} name="profissão">
-          <option value="psicóloga">Psicóloga(o)</option>
-          <option value="psiquiatra">Psiquiatra</option>
-          <option value="outros">Outros</option>
-        </Select>
-        <span> {errors.profissão && errors.profissão.message} </span>
-        <Input
-          label="Certificação"
-          placeholder="CRM ou CFP"
-          register={register}
-          name="crm"
-          {...register("crm")}
-        />
-        <span> {errors.crm && errors.crm.message} </span>
-        <Input
-          label="CNPJ"
-          placeholder="CNPJ"
-          register={register}
-          name="cnpj"
-        />
-        <span> {errors.cnpj && errors.cnpj.message} </span>
-        <ButtonForm type="submit">Cadastrar</ButtonForm>
-      </FormLoginDoctor>
-    </DivGlobalLogin>
+    
   );
 }
 
