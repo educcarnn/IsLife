e91d6afde228ba02d6c2903f64bbb8841365b638
Eduardo <educcarnn@gmail.com>
2022-07-11 21:16:09 -0300

    Feat: Adicionar novos pacientes

--- src/components/FormRegisterNewPatient/index.jsx
+++ src/components/FormRegisterNewPatient/index.jsx
@@ -3,29 +3,44 @@ import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { api } from "../../services/api";
 import { useForm } from "react-hook-form";
-
-// import { Input } from "../../components/inputs";
+import { useState } from "react";
 
 function FormRegisterNewPatient() {
-  const doutor = JSON.parse(localStorage.getItem("@isLifeDoctor:"));
+  const [datesDash, setDatesDash] = useState(
+    JSON.parse(localStorage.getItem("token"))
+  );
+     
+    const sendDates = (info) => {
+      console.log(info)
+      const dates = {
+        name: info.name,
+        email: info.email,
+        age: info.age,
+        cpf: "444.444.444-44",
+        telefone: info.telephone,
+        image: "https://cdn-icons-png.flaticon.com/512/146/146025.png",
+        password: info.password,
+        type: "patient",
+        profissão: "Autonomo",
+        doctorId: datesDash.user.id
+      };
 
-  const token = JSON.parse(localStorage.getItem("@isLifetoken:"));
-
-  console.log(token);
-
-  // console.log(doutor);
-  // console.log(doutor.patients);
+      api
+        .post("/register", dates)
+        .then((res) => console.log(res))
+        .catch((err) => console.log(err));
+    };
+  
 
   const schema = yup.object().shape({
     name: yup.string().required("Campo obrigatório"),
     age: yup.string().required("Campo obrigatório"),
-    cpf: yup.string().required("Campo obrigatório"),
     email: yup.string().email("Email inválido").required("Campo obrigatório"),
     password: yup
       .string()
       .min(8, "Mínimo de 8 digitos")
       .required("Campo obrigatório"),
-    telefone: yup.string().required("Campo obrigatório"),
+    telephone: yup.string().required("Campo obrigatório"),
   });
 
   const {
@@ -36,113 +51,42 @@ function FormRegisterNewPatient() {
     resolver: yupResolver(schema),
   });
 
-  const signUp = (data) => {
-    console.log(data);
-    data.doctorId = `${doutor.id}`;
-    data.image = "";
-    data.profissão = "dev front end";
-    data.type = "patient";
-
-    api
-      .post("/register", data)
-      .then((res) => {
-        console.log(res);
-        localStorage.setItem("@isNewPatient:", JSON.stringify(res.data.user));
-      })
-
-      .catch((err) => console.log(err));
-
-    getPatient();
-  };
-
-  async function getPatient() {
-    const newPatient = JSON.parse(localStorage.getItem("@isNewPatient:"));
-
-    console.log(newPatient);
-
-    const newPatientAdd = {
-      name: newPatient.name,
-      email: newPatient.email,
-      id: newPatient.id,
-    };
-
-    console.log(newPatientAdd);
-    newPatientThisDoctor(newPatientAdd);
-  }
-
-  async function newPatientThisDoctor(obj) {
-    console.log(obj);
-    api
-      .patch(`/users/${doutor.id}`, obj, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      })
-      .then((res) => console.log(res))
-      .catch((err) => console.log(err));
-
-    removeKeyFromPatient();
-  }
-
-  async function removeKeyFromPatient() {
-    localStorage.removeItem("@isNewPatient:");
-  }
 
   return (
-    <ContainerForm onSubmit={handleSubmit(signUp)}>
+    <ContainerForm onSubmit={handleSubmit(sendDates)}>
       <div className="div-input">
         <input
           className="input-new-patient"
-          // label=""
           type="text"
           placeholder="Nome completo"
           {...register("name")}
-          // register={register}
-          // name="name"
         />
         <input
           className="input-new-patient"
-          // label=""
-          type="date"
-          placeholder="Data de Nascimento"
+          placeholder="Idade"
           {...register("age")}
-          // register={register}
-          // name="age"
         />
         <input
           className="input-new-patient"
-          type="text"
-          placeholder="CPF"
-          {...register("cpf")}
-          // register={register}
-          // name="cpf"
-        />
-        <input
-          className="input-new-patient"
-          // label=""
+  
           type="email"
           placeholder="Email"
           {...register("email")}
-          // register={register}
-          // name="email"
+
         />
         <input
           className="input-new-patient"
-          // label=""
           type="password"
           placeholder="Senha"
           {...register("password")}
-          // register={register}
-          // name="password"
         />
         <input
           className="input-new-patient"
-          // label=""
+
           type="tel"
-          {...register("telefone")}
+          {...register("telephone")}
           placeholder="Telefone"
-          // register={register}
-          // name="telefone"
+        
         />
       </div>
       <button type="submit" className="btn-register-patient">
@@ -153,53 +97,3 @@ function FormRegisterNewPatient() {
 }
 
 export default FormRegisterNewPatient;
-
-{
-  /* <Input
-// className="input-new-patient"
-label=""
-type="text"
-placeholder="Nome completo"
-{...register("name")}
-register={register}
-name="name"
-/>
-<Input
-// className="input-new-patient"
-label=""
-type="date"
-placeholder="Data de Nascimento"
-register={register}
-name="age"
-/>
-<Input
-// className="input-new-patient"
-type="text"
-placeholder="CPF"
-register={register}
-name="cpf"
-/>
-<Input
-// className="input-new-patient"
-label=""
-type="email"
-placeholder="Email"
-register={register}
-name="email"
-/>
-<Input
-// className="input-new-patient"
-label=""
-type="password"
-placeholder="Senha"
-register={register}
-name="password"
-/>
-<Input
-// className="input-new-patient"
-label=""
-type="tel"
-placeholder="Telefone"
-register={register}
-/> */
-}
--- src/components/FormRegisterNewPatient/styles.js
+++ src/components/FormRegisterNewPatient/styles.js
@@ -30,6 +30,7 @@ export const ContainerForm = styled.form`
     padding-left: 16px;
   }
   .btn-register-patient {
+    margin-top: -4rem;
     width: 95%;
     height: 50px;
     background-color: #ce2d5d;
