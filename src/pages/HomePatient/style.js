dbc090251f016ae84ddc1add746c96142eed1dad
Eduardo <educcarnn@gmail.com>
2022-07-14 09:34:55 -0300

    Feat: Estilização do css

--- src/pages/HomePatient/style.js
+++ src/pages/HomePatient/style.js
@@ -1,28 +1,58 @@
-import styled from "styled-components";
+import styled, {keyframes} from "styled-components";
+
+const animation = keyframes`
+from{
+ opacity: 0;
+  transform: translateX(50px);
+}
+
+to{
+ opacity: 1;
+  transform: translateX(0px);
+
+}
+`
+
+const anima = keyframes`
+from{
+ opacity: 0;
+}
+
+to{
+ opacity: 1;
+
+}
+`
+
+
 
 export const DivHomePatient = styled.div`
   display: flex;
   flex-direction: column;
-  margin-top: 2rem;
-  gap: 3rem;
+  width: 100vw;
+  height: 100vh;
+  gap: 100px;
   align-items: center;
-  background-color: darkturquoise;
-  padding-bottom: 7%;
-  padding-top: 1.7%;
+  background-color: white;
+  animation: ${animation} 1s;
+
 
   .container-btn--home---patient {
     display: flex;
     width: 100%;
-    justify-content: center;
+    height: 30px;
+    justify-content: flex-end;
 
     .div-btn--home---patient {
       width: 313px;
       display: flex;
       justify-content: flex-end;
       .voltar-home--patient {
-        height: 30px;
-        width: 70px;
-        border-radius: 8px;
+        height: 35px;
+        width: 95px;
+        border-radius: 5px;
+        margin-top: 15px;
+        margin-right: 50px;
       }
     }
   }
@@ -34,11 +64,38 @@ export const DivHomePatient = styled.div`
   .imgHome {
     width: 12rem;
   }
+
   .DivButtonHome {
     display: flex;
     flex-direction: column;
     background-color: #d9d9d9;
     padding: 1rem;
-    border-radius: 5px;
+    border-radius: 2px;
+    width: 247px;
+    height: 68px;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
   }
+
+  .DivButtonHome button {
+    display: flex;
+    flex-direction: column;
+    width: 227px;
+    height: 38px;
+    justify-content: center;
+    align-items: center;
+    font-family: "Inter";
+    background-color: #CE2D5D;
+    color:  white;
+    border: 1px solid #CE2D5D;
+
+
+    :hover{
+      color: #CE2D5D;
+        background-color: white;
+        border: 1px solid #CE2D5D;
+    }
+  }
+
 `;
